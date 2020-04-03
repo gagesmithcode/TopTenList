02ecb1238fc6c7c2dfaaf0fb264a4e97f2977b58
Gage Smith <gage.smith.andrew@gmail.com>
2020-04-03 12:33:30 -0700

    Implemented the IDirectionable interface for TopTenList. Currently displays the Emoticon gallery backwards and then the TopTenList forwards.

--- TopTenList.cpp
+++ TopTenList.cpp
@@ -1,5 +1,6 @@
 //Manages a list of top ten hyperlinks
 #include <string>
+#include <iostream>
 #include "TopTenList.h"
 #include "Hyperlink.h"
 
@@ -7,15 +8,37 @@ using std::string;
 
 TopTenList::TopTenList()
 {
-
+    //Set the size of the list
+    _list.resize(11);
 }
 
 void TopTenList::set_at(int index, Hyperlink link)
 {
-
+    //Place the link at the specific index in the list
+    _list[index] = link;
 }
 
 Hyperlink TopTenList::get(int index)
 {
-
+    if (index >= 0 && index < _list.size())
+    {
+        return _list[index];
+    }
+    else
+    {
+        return Hyperlink();
+    }
 }
+
+void TopTenList::display_forward()
+{
+    for(int i = 1; i < _list.size(); i++)
+    {
+        std::cout << i << ". " << "<" << _list[i].text << "> " << _list[i].url << std::endl;
+    }
+}
+
+// void TopTenList::display_backward()
+// {
+
+// }
